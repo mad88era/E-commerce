ac3aa32646c7ad64dd694f2e246f52a5415f09a3
mad88era <150746505+mad88era@users.noreply.github.com>
2024-03-28 22:21:46 -0700

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,39 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  OnDelete: 'CASCADE'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  OnDelete: 'CASCADE'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  OnDelete: 'CASCADE'
+})
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
